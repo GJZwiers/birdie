efa5ab74f4f6d304a6ef84f41fb5fb0e13ed76dc
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-09-06 22:04:53 +0200

    chore: cleanup test code

--- deps.ts
+++ deps.ts
@@ -1,3 +1,1 @@
-import { transcribe } from "https://deno.land/x/birdie@0.1.2/mod.ts";
-
-transcribe("ACGT");
+export {};
--- mods/mod.ts
+++ mods/mod.ts
@@ -1,14 +1,14 @@
 import init, { transcribe } from "./dna/pkg/dna.js";
 
-const wasm = await fetch(
-  new URL("./dna/pkg/dna_bg.wasm", import.meta.url).href,
-  {
-    headers: {
-      "content-type": "application/wasm",
-    },
-  },
-);
+// const wasm = await fetch(
+//   new URL("./dna/pkg/dna_bg.wasm", import.meta.url).href,
+//   {
+//     headers: {
+//       "content-type": "application/wasm",
+//     },
+//   },
+// );
 
-await init(wasm);
+await init();
 
 export { transcribe };
