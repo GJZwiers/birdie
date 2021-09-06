ec339c751ff157fe93c3a9b9c874bb5f0609438f
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-09-06 20:01:31 +0200

    fix: pass href to deno.readfile

--- mods/mod.ts
+++ mods/mod.ts
@@ -1,7 +1,7 @@
 import init, { transcribe } from "./dna/pkg/dna.js";
 
 const wasm = await Deno.readFile(
-  new URL("./dna/pkg/dna_bg.wasm", import.meta.url),
+  new URL("./dna/pkg/dna_bg.wasm", import.meta.url).href,
 );
 
 /** Wrapping the file in a Response object triggers loading with the more efficient `instantiateStreaming`. */
