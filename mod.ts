df3160e06146bce6e8a381405b31fc203e23bcef
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-09-16 20:32:05 +0200

    fix: include init fn in mod.ts

--- mod.ts
+++ mod.ts
@@ -1,9 +1,1 @@
-// export { levenshtein, myers, hamming, transcribe } from "./mods/mod.ts";
-import * as distance from "./mods/mod.ts";
-
-await distance.initWasm(Deno.readFile("./mods/levenshtein/pkg/levenshtein_bg.wasm"));
-
-const x = "GTCTGCATGCG";
-const y = "TTTAGCTAGCG";
-
-console.log(distance.hamming(x, y));
+export { initWasm, levenshtein, myers, hamming } from "./mods/mod.ts";
