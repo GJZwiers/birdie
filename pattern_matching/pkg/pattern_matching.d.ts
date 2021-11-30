8d2f0648d521584eff76d812ea98fa65f7136d79
Geert-Jan Zwiers <34610306+GJZwiers@users.noreply.github.com>
2021-11-30 20:47:17 +0100

    chore(docs): add installation paragraph (#7)
    * chore(docs): add installation paragraph

--- pattern_matching/pkg/pattern_matching.d.ts
+++ pattern_matching/pkg/pattern_matching.d.ts
@@ -1,51 +1,68 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
-* ShiftAnd algorithm for pattern matching. Patterns may contain at most 64 symbols.
-* Complexity: O(n) with text length n.
-* @param {string} pattern
-* @param {string} text
-* @returns {number}
-*/
+ * ShiftAnd algorithm for pattern matching. Patterns may contain at most 64 symbols.
+ * Complexity: O(n) with text length n.
+ * @param {string} pattern
+ * @param {string} text
+ * @returns {number}
+ */
 export function shift_and(pattern: string, text: string): number;
 /**
-* Algorithm of Knuth, Morris and Pratt.
-* @param {string} pattern
-* @param {string} text
-* @returns {Uint32Array}
-*/
+ * Algorithm of Knuth, Morris and Pratt.
+ * @param {string} pattern
+ * @param {string} text
+ * @returns {Uint32Array}
+ */
 export function kmp(pattern: string, text: string): Uint32Array;
 /**
-* Algorithm of Horspool. Window-based, similar to but faster than Boyer-Moore.
-* @param {string} pattern
-* @param {string} text
-* @returns {Uint32Array}
-*/
+ * Algorithm of Horspool. Window-based, similar to but faster than Boyer-Moore.
+ * @param {string} pattern
+ * @param {string} text
+ * @returns {Uint32Array}
+ */
 export function horspool(pattern: string, text: string): Uint32Array;
 /**
-* Backward oracle matching algorithm.
-* @param {string} pattern
-* @param {string} text
-* @returns {Uint32Array}
-*/
+ * Backward oracle matching algorithm.
+ * @param {string} pattern
+ * @param {string} text
+ * @returns {Uint32Array}
+ */
 export function bom(pattern: string, text: string): Uint32Array;
 /**
-* Backward nondeterministic DAWG matching (BNDM).
-* @param {string} pattern
-* @param {string} text
-* @returns {Uint32Array}
-*/
+ * Backward nondeterministic DAWG matching (BNDM).
+ * @param {string} pattern
+ * @param {string} text
+ * @returns {Uint32Array}
+ */
 export function bndm(pattern: string, text: string): Uint32Array;
 
-export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+export type InitInput =
+  | RequestInfo
+  | URL
+  | Response
+  | BufferSource
+  | WebAssembly.Module;
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly shift_and: (a: number, b: number, c: number, d: number) => number;
   readonly kmp: (a: number, b: number, c: number, d: number, e: number) => void;
-  readonly horspool: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly horspool: (
+    a: number,
+    b: number,
+    c: number,
+    d: number,
+    e: number,
+  ) => void;
   readonly bom: (a: number, b: number, c: number, d: number, e: number) => void;
-  readonly bndm: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly bndm: (
+    a: number,
+    b: number,
+    c: number,
+    d: number,
+    e: number,
+  ) => void;
   readonly __wbindgen_malloc: (a: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
@@ -53,11 +70,13 @@ export interface InitOutput {
 }
 
 /**
-* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
-* for everything else, calls `WebAssembly.instantiate` directly.
-*
-* @param {InitInput | Promise<InitInput>} module_or_path
-*
-* @returns {Promise<InitOutput>}
-*/
-export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {InitInput | Promise<InitInput>} module_or_path
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function init(
+  module_or_path?: InitInput | Promise<InitInput>,
+): Promise<InitOutput>;
