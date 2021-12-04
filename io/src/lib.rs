4275b557d2e1b3775d2d1362067f81dfd82150bb
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-12-04 10:51:58 +0100

    feat(io): read fasta file

---
+++ io/src/lib.rs
@@ -0,0 +1,32 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+use seq_io::fasta::{Reader, Record};
+use std::io;
+
+extern crate console_error_panic_hook;
+use std::panic;
+
+// For printing Rust errors in the JS context
+fn my_init_function() {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+}
+
+#[wasm_bindgen(method)]
+pub fn read_fasta_file(filename: &str) {
+    my_init_function();
+    let mut reader = Reader::from_path(filename).unwrap(); //fasta::Reader::from_file(filename).unwrap();
+
+    let mut stdout = io::stdout();
+    
+    while let Some(result) = reader.next() {
+        let record = result.unwrap();
+        // determine sequence length
+        let seqlen = record.seq_lines()
+                           .fold(0, |l, seq| l + seq.len());
+
+        if seqlen > 100 {
+            record.write_wrap(&mut stdout, 80).unwrap();
+        } else {
+            eprintln!("{} is only {} long", record.id().unwrap(), seqlen);
+        }
+    }
+}
